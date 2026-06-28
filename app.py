aaefb9fcacbf996d036e01d128e68decc62a9d9d
23030100-cyber <23030100@student.bdu.edu.vn>
2026-06-28 15:52:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.image("logo.jpg")
 # TIÊU ĐỀ
 # =============================
 st.title("📈 TRỰC QUAN HÓA GIÁ CỔ PHIẾU VÀ KIỂM ĐỊNH MANN-KENDALL")
-st.subheader("Lê Thị Hảo")
+st.subheader("Lê Thị Hảo - đề tài 8")
 
 st.markdown("---")
 
